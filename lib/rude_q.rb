05ad28a6cbdcc25e2a9f75cfbcba43ff2adcc287
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2008-08-07 21:52:50 +0100

    better documentation

--- lib/rude_q.rb
+++ lib/rude_q.rb
@@ -1,8 +1,16 @@
 # RudeQ
 require 'digest/sha1'
+
+# simply doing;
+#   class RudeQueue < ActiveRecord::Base
+#     include RudeQ
+#   end
+# will include RudeQ::ClassMethods
+#   :get
+#   :set
 module RudeQ
 
-  def self.included(mod)
+  def self.included(mod) # :nodoc:
     mod.extend(ClassMethods)
     mod.serialize(:data)
   end
@@ -16,17 +24,17 @@ module RudeQ
       self.delete_all(["processed = ? AND updated_at < ?", true, expiry.to_i.ago])
     end
   
-    # Add any serialize-able *data* to the queue *queue_name* (strings and symbols are treated the same)
+    # Add any serialize-able +data+ to the queue +queue_name+ (strings and symbols are treated the same)
     #
     #   RudeQueue.set(:sausage_queue, Sausage.new(:sauce => "yummy"))
     #   RudeQueue.set("sausage_queue", Sausage.new(:other => true))
     #
-    #     RudeQueue.get("sausage_queue")
-    #     -> *yummy sausage*
-    #     RudeQueue.get(:sausage_queue)
-    #     -> *other_sausage*
-    #     RudeQueue.get(:sausage_queue)
-    #     -> nil
+    #   >> RudeQueue.get("sausage_queue")
+    #   -> *yummy sausage*
+    #   >> RudeQueue.get(:sausage_queue)
+    #   -> *other_sausage*
+    #   >> RudeQueue.get(:sausage_queue)
+    #   -> nil
     def set(queue_name, data)
       queue_name = sanitize_queue_name(queue_name)
       self.create!(:queue_name => queue_name, :data => data)
@@ -34,16 +42,16 @@ module RudeQ
     end
 
     # Grab the first item from the queue *queue_name* (strings and symbols are treated the same)
-    # - it should always come out the same as it went in
-    # - they should always come out in the same order they went in
-    # - it will return a nil if there is no unprocessed entry in the queue
+    #   - it should always come out the same as it went in
+    #   - they should always come out in the same order they went in
+    #   - it will return a nil if there is no unprocessed entry in the queue
     #
-    #  RudeQueue.get(21)
-    #  -> {:a => "hash"}
-    #  RudeQueue.get(:a_symbol)
-    #  -> 255
-    #  RudeQueue.get("a string")
-    #  -> nil
+    #   >> RudeQueue.get(21)
+    #   -> {:a => "hash"}
+    #   >> RudeQueue.get(:a_symbol)
+    #   -> 255
+    #   >> RudeQueue.get("a string")
+    #   -> nil
     def get(queue_name)
       qname = sanitize_queue_name(queue_name)
       
@@ -57,12 +65,6 @@ module RudeQ
       end
     end
 
-    # TODO: consider moving to a simple ActiveRecord :lock => true
-    #   queued = self.find_by_queue_name_and_processed(qname, false, :lock => true)
-    # in innodb this gives us a nice row lock
-    #
-    # if we use a transaction with the current tokenised lock
-    # its equivalent to a table lock == :bad
     def fetch_with_lock(qname, &block) # :nodoc:
       RudeQ::PessimisticLock.fetch_with_lock(self, qname, &block)
     end
@@ -81,6 +83,11 @@ module RudeQ
     end
   end
   
+  # uses standard ActiveRecord :lock => true
+  # this will invoke a lock on the particular queue
+  #   eg. daemon1: RudeQueue.get(:abc)
+  #       daemon2: RudeQueue.get(:abc) - will have to wait for daemon1 to finish
+  #       daemon3: RudeQueue.get(:def) - will avoid the lock
   module PessimisticLock
     class << self
       
@@ -96,7 +103,14 @@ module RudeQ
     
     end
   end
-
+  
+  # a crazy hack around database locking
+  # that I thought was a good idea
+  # turns out we can't make it use transactions properly
+  # without creating a whole table lock
+  # which misses the point
+  #
+  # also, it doesn't work on SQLite as it requires "UPDATE ... LIMIT 1 ORDER id ASC"
   module TokenLock
     class << self
       
